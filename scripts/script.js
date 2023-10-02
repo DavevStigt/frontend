bb612df94300a32a346537dc2bb3aecafb533d88
DavevStigt <144068761+DavevStigt@users.noreply.github.com>
2023-10-03 01:34:22 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
 // JavaScript Document
 console.log("hi");
+
+
+function rightScroll() {
+    const left = document.querySelector(".trending-deze-week ul");
+    left.scrollBy(300, 0);
+}
+function leftScroll() {
+    const right = document.querySelector(".trending-deze-week ul");
+    right.scrollBy(-300, 0);
+}
+scrollBy(x - coord, y - coord)
